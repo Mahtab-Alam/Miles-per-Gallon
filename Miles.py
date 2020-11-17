9e723259b4d015fda268d6ca8b7489b8501fb9ae
mahtab alam <ma1937835@gmail.com>
2020-11-17 13:59:35 +0530

    Calculate Car average miles/gallon

---
+++ Miles.py
@@ -0,0 +1,12 @@
+milesDistance = float( input("Please enter the number of miles distance: " ))
+gallonsOfGasused = float(input("Please enter the gallons of gas used: "))
+milesPerGallon = milesDistance / gallonsOfGasused
+print("The car's miles-per-gallon is " + str(milesPerGallon) + " miles/gallon" )
+
+# Chapter 2 programming challenges
+# 7. Milles-per-Gallon
+# A car's miles-per-gallon (MPG) can be calculated with the following formula:
+# MGP = Miles distance / Gallons of gas used
+# Write a program that asks the user for the number of miles distance and the
+# gallons of gas used, It should calculate the car's MGP and desplay the result,
+#Starting out with Pthon, Third Edition, Tony Gaddis
